7168c5a77f0048cd1593baca69d2d7efd2ff00bd
Apoorva022000 <128361967+Apoorva022000@users.noreply.github.com>
2023-04-27 16:19:57 +0530

    updating app.test

--- src/App.test.js
+++ src/App.test.js
@@ -1,8 +1,8 @@
 import { render, screen } from '@testing-library/react';
 import App from './App';
 
-test('renders learn react link', () => {
+test('renders a div with a specific class', () => {
   render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
+  const divElement = screen.getByTestId('specific-div');
+  expect(divElement).toBeInTheDocument();
 });
