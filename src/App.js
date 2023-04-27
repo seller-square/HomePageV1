820072258f84a7817e77ec5503f037c1ccc5b9ed
Apoorva022000 <128361967+Apoorva022000@users.noreply.github.com>
2023-04-27 16:12:40 +0530

    updating ui

--- src/App.js
+++ src/App.js
@@ -1,10 +1,10 @@
 import styles from './index.css';
-import React, { useState, useEffect } from 'react';
+import React, { useState } from 'react';
 // test
 function App() {
   const [popup, setpopup] = useState(false);
   const [submitted, setSubmitted] = useState(false);
-  const[submit, setsubmit] = useState(true);
+  const [submit, setsubmit] = useState(true);
 
   const handleSubmit = async (e) => {
     e.preventDefault();
@@ -22,36 +22,65 @@ function App() {
   return (
     <div className={styles.App}>
       {submit &&
-      <div>
-      <div className="">
-        <img className='logo' src="/logo.png" alt="Company Logo" />
-      </div>
-      <div>
-        <div>
-          <h1 className='signIn'>Sign In</h1>
-          <form action="" className='signin-form' onSubmit={handleSubmit}>
-            <input type="email" className='form-inp-1' placeholder='example.email@gmail.com' />
-            <input type="password" className='form-inp-2' placeholder='Enter at least 8+ characters' />
-            <div>
+        <div className=''>
+          <div className="">
+            <img className='logo' src="/logo.png" alt="Company Logo" />
+          </div>
+          <div className='main'>
+            <div className='form-fields'>
               <div>
-                <label className='label-one'>Remember Me</label>
-                <input type="checkbox" className='form-inp-3'></input>
-                <a href="" className='forgot-pswd' >Forgot password?</a>
+                <div>
+                  <h1 className='signIn'>Sign In</h1>
+                  <div>
+                    <form action="" className='signin-form' onSubmit={handleSubmit}>
+                      <div className='inputs'>
+                        <div>
+                          <input type="email" className='form-inp' placeholder='example.email@gmail.com' />
+                        </div>
+                        <div>
+                          <input type="password" className='form-inp' placeholder='Enter at least 8+ characters' />
+                        </div>
+                      </div>
+
+                      <button type='submit' className='signin-btn'>Sign In</button>
+                    </form>
+                  </div>
+                </div>
               </div>
             </div>
-            <button type='submit' className='signin-btn'>Sign In</button>
-          </form>
-        </div>
-        <div className='container'>
-          <h1 className='heading'>The Most Comprehensive Amazon Profit Calculator</h1>
-          <p className='para-1'>Calculate precise Margins Across Local, Regional and Naional Regions</p>
-          <p className='para-2'>Simulate Profits at Different Prices</p>
-          <p className='para-3'>Simulate Profits at Different Discounts</p>
-          <a href="http://app.sellersquare.in/free-ecommerce-tools/margin-analyser/amazon-margin-calculator"><button className='calculator-btn' type="submit"> Free Amazon Profit Calculator </button></a>
-        </div>
-      </div>
+            <div className='main-cnt'>
+            <div className='container'>
+              <h1 className='heading'>The Most Comprehensive Amazon Profit Calculator</h1>
+              <div className='box'>
+                <div className='box-1'>
+                  <icon className="icon">	&#10003;</icon>
+                </div>
+                <div className='box-2'> 
+                  <p className='para'>Calculate precise Margins Across Local, Regional and National Regions</p>
+                </div>
+              </div>
+              <div className='box'>
+                <div className='box-1'>
+                  <icon className="icon">	&#10003;</icon>
+                </div>
+                <div className='box-2'>
+                  <p className='para'>Simulate Profits at Different Prices</p>
+                </div>
+              </div>
+              <div className='box'>
+                <div className='box-1'>
+                  <icon className="icon">	&#10003;</icon>
+                </div>
+                <div className='box-2'>
+                  <p className='para'>Simulate Profits at Different Discounts</p>
+                </div>
+              </div>
+              <a href="http://app.sellersquare.in/free-ecommerce-tools/margin-analyser/amazon-margin-calculator"><button className='calculator-btn' type="submit"> Free Amazon Profit Calculator </button></a>
+            </div>
           </div>
-}
+        </div>
+        </div>
+      }
       {submitted &&
         <div className='hidden'>
           <div className='popup'>
