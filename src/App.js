b5cdb56f85b6db02e77f2eb6d1c25292bd4ea7d8
Apoorva022000 <128361967+Apoorva022000@users.noreply.github.com>
2023-04-28 16:04:04 +0530

    updating responsive

--- src/App.js
+++ src/App.js
@@ -10,19 +10,20 @@ function App() {
     e.preventDefault();
     setSubmitted(true);
     setsubmit(true);
-  //  setpopup(true);
+    //  setpopup(true);
   };
 
   const handleCancel = () => {
     setsubmit(true);
-   // setpopup(false);
+    // setpopup(false);
     setSubmitted(false);
   };
 
   return (
-        <div className={styles.App}>
-       <div data-testid="specific-div" className="App">
-          </div>
+
+    <div className={styles.App}>
+      <div data-testid="specific-div" className="App">
+      </div>
       {submit &&
         <div className=''>
           <div className="">
@@ -45,42 +46,50 @@ function App() {
                       </div>
 
                       <button type='submit' className='signin-btn'>Sign In</button>
+
                     </form>
+
+                    <a href="http://margin-analyzer.sellersquare.in/"><button className='calculator-btn-2' type="submit"> Free Amazon Profit Calculator </button></a>
+
                   </div>
                 </div>
               </div>
             </div>
+
             <div className='main-cnt'>
-            <div className='container'>
-              <h1 className='heading'>The Most Comprehensive Amazon Profit Calculator</h1>
-              <div className='box'>
-                <div className='box-1'>
-                  <icon className="icon">	&#10003;</icon>
+              <div className='container'>
+                <h1 className='ss-heading'>Seller Square</h1>
+                <p className='beta'>Beta Version</p>
+                <h1 className='heading'>The Most Comprehensive Amazon Profit Calculator</h1>
+                <div className='box'>
+                  <div className='box-1'>
+                  <img className='icon' src="/check-mark.png" alt="Company Logo" />
+                  </div>
+                  <div className='box-2'>
+                    <p className='para'>Simulate profits at different selling prices on Amazon to <br></br> optimise pricing</p>
+                  </div>
                 </div>
-                <div className='box-2'> 
-                  <p className='para'>Calculate precise Margins Across Local, Regional and National Regions</p>
+                <div className='box'>
+                  <div className='box-1'>
+                  <img className='icon' src="/check-mark.png" alt="Company Logo" />
+                  </div>
+                  <div className='box-2'>
+                    <p className='para'>Calculate precise margins on Amazon across local, regional<br/> and national regions</p>
+                  </div>
                 </div>
+                <div className='box'>
+                  <div className='box-1'>
+                  <img className='icon' src="/check-mark.png" alt="Company Logo" />
+
+                  </div>
+                  <div className='box-2'>
+                    <p className='para'>Simulate profits at different discounts to participate in<br/> Amazon deals and Coupons</p>
+                  </div>
+                </div>
+                <a href="http://margin-analyzer.sellersquare.in/"><button className='calculator-btn' type="submit"> Free Amazon Profit Calculator </button></a>
               </div>
-              <div className='box'>
-                <div className='box-1'>
-                  <icon className="icon">	&#10003;</icon>
-                </div>
-                <div className='box-2'>
-                  <p className='para'>Simulate Profits at Different Prices</p>
-                </div>
-              </div>
-              <div className='box'>
-                <div className='box-1'>
-                  <icon className="icon">	&#10003;</icon>
-                </div>
-                <div className='box-2'>
-                  <p className='para'>Simulate Profits at Different Discounts</p>
-                </div>
-              </div>
-              <a href="http://app.sellersquare.in/free-ecommerce-tools/margin-analyser/amazon-margin-calculator"><button className='calculator-btn' type="submit"> Free Amazon Profit Calculator </button></a>
             </div>
           </div>
-        </div>
         </div>
       }
       {submitted &&
